f69a12bb2a88237085c7d749ef96c855cb93cc84
Shveta Puri <shvetapuri@Shvetas-MacBook-Pro-2.local>
2019-09-20 10:24:08 -0700

    with container views switching, with segemented control at top of master view

--- tb_fb/Controller/MasterViewController.swift
+++ tb_fb/Controller/MasterViewController.swift
@@ -12,42 +12,12 @@ class MasterViewController: UIViewController {
 
     @IBOutlet weak var segmentedControl: UISegmentedControl!
 
-    private lazy var addViewController: AddViewController = {
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        var viewController = storyboard.instantiateViewController(withIdentifier: "AddViewController") as! AddViewController
-        // Add the view controller to the container.
-        viewController.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-        addChild(viewController)
-
-        view.addSubview(viewController.view)
-            
-        viewController.didMove(toParent: self)
-            
-        return viewController
-            
-        }()
-    private lazy var searchViewContoller: SearchViewController = {
-
-        // Create a child view controller and add it to the current view controller.
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        var viewController = storyboard.instantiateViewController(withIdentifier: "SearchViewController") as! SearchViewController
-            // Add the view controller to the container.
-        viewController.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-        addChild(viewController)
-
-        view.addSubview(viewController.view)
-            
-            // Notify the child view controller that the move is complete.
-        viewController.didMove(toParent: self)
-        return viewController
-       
-    }()
-        
+    @IBOutlet weak var searchContainerView: UIView!
+    @IBOutlet weak var addContainerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        addVC(asChildViewController: searchViewContoller)
-
+        
         setupSegmentedControl()
     }
 
@@ -64,71 +34,28 @@ class MasterViewController: UIViewController {
     
     @IBAction func segmentedControlTapped(_ sender: Any) {
         if segmentedControl.selectedSegmentIndex == 0 {
-            remove(asChildViewController: addViewController)
-            addVC(asChildViewController: searchViewContoller)
+            UIView.animate(withDuration: 0.5, animations: {
+               // self.searchContainerView.alpha = 1
+                //self.addContainerView.alpha = 0
+                self.searchContainerView.isHidden = false
+                self.addContainerView.isHidden = true
+            })
+            
+            //remove(asChildViewController: addViewController)
+          //  addVC(asChildViewController: searchViewContoller)
         } else {
-            remove(asChildViewController: searchViewContoller)
-            addVC(asChildViewController: addViewController)
+            UIView.animate(withDuration: 0.5, animations: {
+                // self.searchContainerView.alpha = 1
+                //self.addContainerView.alpha = 0
+                self.searchContainerView.isHidden = true
+                self.addContainerView.isHidden = false
+            })
+        //    remove(asChildViewController: searchViewContoller)
+         //   addVC(asChildViewController: addViewController)
         }
     }
     
     
-//    func setupSearchViewController() {
-//        // Create a child view controller and add it to the current view controller.
-//        let storyboard = UIStoryboard(name: "Main", bundle: .main)
-//        if let viewController = storyboard.instantiateViewController(withIdentifier: "SearchViewContoller")
-//            as? SearchViewController {
-//            // Add the view controller to the container.
-//            addChild(viewController)
-//            view.addSubview(viewController.view)
-//
-//            // Create and activate the constraints for the child’s view.
-//            //onscreenConstraints = configureConstraintsForContainedView(containedView: viewController.view,
-//                                                                    //   stage: .onscreen)
-//           // NSLayoutConstraint.activate(onscreenConstraints)
-//
-//            // Notify the child view controller that the move is complete.
-//            viewController.didMove(toParent: self)
-//            searchViewContoller = viewController
-//        }
-//
-//    }
-    private func addVC(asChildViewController: UIViewController) {
-        addChild(asChildViewController)
-        view.addSubview(asChildViewController.view)
-        asChildViewController.didMove(toParent: self)
-    }
-//    func setupAddViewController () {
-//        // Create a child view controller and add it to the current view controller.
-//        let storyboard = UIStoryboard(name: "Main", bundle: .main)
-//        if let viewController = storyboard.instantiateViewController(withIdentifier: "AddViewContoller")
-//            as? AddViewController {
-//            // Add the view controller to the container.
-//            addChild(viewController)
-//            view.addSubview(viewController.view)
-//
-//            // Create and activate the constraints for the child’s view.
-//            //onscreenConstraints = configureConstraintsForContainedView(containedView: viewController.view,
-//            //   stage: .onscreen)
-//            // NSLayoutConstraint.activate(onscreenConstraints)
-//
-//            // Notify the child view controller that the move is complete.
-//            viewController.didMove(toParent: self)
-//            addViewController = viewController
-//        }
-//
-//    }
-    
-    private func remove(asChildViewController: UIViewController) {
-        // Notify Child View Controller
-        asChildViewController.willMove(toParent: nil)
-        
-        // Remove Child View From Superview
-        asChildViewController.view.removeFromSuperview()
-        
-        // Notify Child View Controller
-        asChildViewController.removeFromParent()
-    }
     
     
 }
--- tb_fb/MasterTableViewController.swift
+++ tb_fb/MasterTableViewController.swift
@@ -200,3 +200,103 @@ extension MasterTableViewController: UISearchResultsUpdating {
         // TODO
     }
 }
+/*
+ switching views in  container views with segmented control in navigation bar
+ //    private lazy var addViewController: AddViewController = {
+ 
+ //        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+ //        var viewController = storyboard.instantiateViewController(withIdentifier: "AddViewController") as! AddViewController
+ //        // Add the view controller to the container.
+ //        viewController.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+ //        addChild(viewController)
+ //
+ //        view.addSubview(viewController.view)
+ //
+ //        viewController.didMove(toParent: self)
+ //
+ //        return viewController
+ //
+ //        }()
+ //    private lazy var searchViewContoller: SearchViewController = {
+ //
+ //        // Create a child view controller and add it to the current view controller.
+ //       // let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+ //       // var viewController = storyboard.instantiateViewController(withIdentifier: "SearchViewController") as! SearchViewController
+ //            // Add the view controller to the container.
+ //        //viewController.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+ //        let viewController = children.first as? SearchViewController
+ //
+ //        addChild(viewController!)
+ //
+ //        view.addSubview(viewController!.view)
+ //
+ //            // Notify the child view controller that the move is complete.
+ //        viewController!.didMove(toParent: self)
+ //        return viewController!
+ //
+ //
+ //    }()
+
+ //viewdidload() {
+ // Do any additional setup after loading the view, typically from a nib.
+ //addVC(asChildViewController: searchViewContoller)
+ //}
+ 
+ //    func setupSearchViewController() {
+ //        // Create a child view controller and add it to the current view controller.
+ //        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+ //        if let viewController = storyboard.instantiateViewController(withIdentifier: "SearchViewContoller")
+ //            as? SearchViewController {
+ //            // Add the view controller to the container.
+ //            addChild(viewController)
+ //            view.addSubview(viewController.view)
+ //
+ //            // Create and activate the constraints for the child’s view.
+ //            //onscreenConstraints = configureConstraintsForContainedView(containedView: viewController.view,
+ //                                                                    //   stage: .onscreen)
+ //           // NSLayoutConstraint.activate(onscreenConstraints)
+ //
+ //            // Notify the child view controller that the move is complete.
+ //            viewController.didMove(toParent: self)
+ //            searchViewContoller = viewController
+ //        }
+ //
+ //    }
+ private func addVC(asChildViewController: UIViewController) {
+ addChild(asChildViewController)
+ view.addSubview(asChildViewController.view)
+ asChildViewController.didMove(toParent: self)
+ }
+ //    func setupAddViewController () {
+ //        // Create a child view controller and add it to the current view controller.
+ //        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+ //        if let viewController = storyboard.instantiateViewController(withIdentifier: "AddViewContoller")
+ //            as? AddViewController {
+ //            // Add the view controller to the container.
+ //            addChild(viewController)
+ //            view.addSubview(viewController.view)
+ //
+ //            // Create and activate the constraints for the child’s view.
+ //            //onscreenConstraints = configureConstraintsForContainedView(containedView: viewController.view,
+ //            //   stage: .onscreen)
+ //            // NSLayoutConstraint.activate(onscreenConstraints)
+ //
+ //            // Notify the child view controller that the move is complete.
+ //            viewController.didMove(toParent: self)
+ //            addViewController = viewController
+ //        }
+ //
+ //    }
+ 
+ private func remove(asChildViewController: UIViewController) {
+ // Notify Child View Controller
+ asChildViewController.willMove(toParent: nil)
+ 
+ // Remove Child View From Superview
+ asChildViewController.view.removeFromSuperview()
+ 
+ // Notify Child View Controller
+ asChildViewController.removeFromParent()
+ }
+
+ */
